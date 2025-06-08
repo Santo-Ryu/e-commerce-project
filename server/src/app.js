39fa40efd21b808d86427201c6a0d3312abd371b
santo_ryu <loithanhle2202@gmail.com>
2025-06-08 15:23:25 +0700

    feat: define middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
 const app = express();
+const errorHandler = require('./middlewares/error.middleware');
+const logger = require('./middlewares/logger.middleware');
 
+// Middlerware xử lý JSON
 app.use(express.json());
 
-app.get('/', (request, response) => {
+// Route chính
+app.get('/ecommerce', (request, response) => {
   response.send('Hello from Express App!');
+});
+
+app.use(logger);
+
+// Middleware lý bắt lỗi các router phía trên
+app.use((err, req, res, next) => {
+  errorHandler(err, req, res, next);
 });
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,16 @@ const {testConnection} = require('./config/db');
 const PORT = process.env.SERVER_PORT || 3000;
 const HOST = process.env.SERVER_ADDRESS || '0.0.0.0';
 
-testConnection().then(() => {
-  app.listen(PORT, HOST, () => {
-    console.log(`🚀 Server is running at http://${HOST}:${PORT}`);
-  });
-});
+const startServer = async () => {
+  try {
+    await testConnection();
+    app.listen(PORT, HOST, () => {
+    console.log(`🚀 Server is running at http://${HOST}:${PORT}/ecommerce`);
+    });
+  }catch(err) {
+    console.error('❗ Database connection failed:', error);
+    process.exit(1); // Thoát ứng dụng nếu kết nối DB thất bại
+  }
+}
+
+startServer();
